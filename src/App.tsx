f22349f80a9cc8e8deecb6e842cafb26929b5e71
Steven Bautista <stevenmb1995@gmail.com>
2024-11-29 13:04:51 -0600

    added state to control color, added util function to translate objects vertically

--- src/App.tsx
+++ src/App.tsx
@@ -4,79 +4,99 @@ import {
   PerspectiveCamera,
   OrbitControls,
   Environment,
-  useTexture,
-  useGLTF,
 } from "@react-three/drei";
 import { useFrame, useThree, Canvas } from "@react-three/fiber";
 import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
-import { Button } from "./components/ui/button";
+
 function App() {
   const orbitControlsRef = useRef(null);
   const sphereRef = useRef(null);
-  const [message,setMessage]=useState("")
+  const [message, setMessage] = useState("");
+  const [redState, setRedState] = useState({ clicked: false, color:"#C15151", toggledColor:"#491717"  });
+  const [greenState, setGreenState] = useState({ clicked: false, color:"#a7f2a7", toggledColor:"#254917" });
+  const [blueState, setBlueState] = useState({ clicked: false, color:"#98F5F9", toggledColor:"#0A1A70"  });
 
   // Raycasting logic
   const RaycasterHandler = () => {
     const { camera, scene, gl } = useThree();
     const raycaster = useRef(new THREE.Raycaster());
     const pointer = useRef(new THREE.Vector2());
-  
     useEffect(() => {
       const handlePointerMove = (event: MouseEvent) => {
         // Convert mouse coordinates to normalized device coordinates (-1 to +1)
         pointer.current.x = (event.clientX / gl.domElement.clientWidth) * 2 - 1;
-        pointer.current.y = -(event.clientY / gl.domElement.clientHeight) * 2 + 1;
+        pointer.current.y =
+          -(event.clientY / gl.domElement.clientHeight) * 2 + 1;
       };
-  
       window.addEventListener("mousemove", handlePointerMove);
-  
       return () => {
         window.removeEventListener("mousemove", handlePointerMove);
       };
     }, [gl.domElement]);
-  
     useFrame(() => {
       // Update the raycaster based on the latest pointer and camera
       raycaster.current.setFromCamera(pointer.current, camera);
     });
-  
+
     useEffect(() => {
       const handleClick = () => {
         // Use the updated raycaster
-        const intersects = raycaster.current.intersectObjects(scene.children, true);
-  
+        const intersects = raycaster.current.intersectObjects(
+          scene.children,
+          true
+        );
+
         if (intersects.length > 0) {
           const clickedObject = intersects[0].object;
-        setMessage(`You clicked on: ${clickedObject.name}`);
-        console.log("Clicked on:", clickedObject.name);
+          setMessage(`You clicked on: ${clickedObject.name}`);
 
-        if(clickedObject.name!="floor"){
-          gsap.to(clickedObject.position, {
-            y: clickedObject.position.y + 1, // Lift the sphere by 1 unit
-            duration: 0.5,
-            ease: "power1.out",
-          });
-        }
-
-       
+          switch (clickedObject.name) {
+            case "floor":
+              break;
+            case "Red ball":
+              if (redState.clicked) {
+                transObjY(clickedObject, -1);
+                setRedState({ ...redState, clicked: false });
+              } else {
+                transObjY(clickedObject, 1);
+                setRedState({ ...redState, clicked: true });
+              }
+              break;
+            case "Blue ball":
+              if (blueState.clicked) {
+                transObjY(clickedObject, -1);
+                setBlueState({ ...blueState, clicked: false });
+              } else {
+                transObjY(clickedObject, 1);
+                setBlueState({ ...blueState, clicked: true });
+              }
+              break;
+            case "Green ball":
+              if (greenState.clicked) {
+                transObjY(clickedObject, -1);
+                setGreenState({ ...greenState, clicked: false });
+              } else {
+                transObjY(clickedObject, 1);
+                setGreenState({ ...greenState, clicked: true });
+              }
+              break;
+          }
         }
       };
-  
+
       window.addEventListener("click", handleClick);
-  
       return () => {
         window.removeEventListener("click", handleClick);
       };
     }, [scene]);
-  
+
     return null;
   };
-  
 
   return (
     <>
-      <div className="border-red-600 border-4 border-solid flex justify-center items-center min-w-0 w-screen h-screen">
+      <div className="flex flex-col md:flex-row justify-center items-center min-w-0 w-screen h-screen bg-slate-300">
         <Canvas id="three-canvas-container" shadows className="flex-1">
           <RaycasterHandler />
           <PerspectiveCamera makeDefault position={[0, 1, 5]} />
@@ -85,25 +105,41 @@ function App() {
             minPolarAngle={anglesToRadians(60)}
             maxPolarAngle={anglesToRadians(80)}
           />
-
-          <mesh position={[0, 0.5, 0]} castShadow ref={sphereRef} name="Blue ball">
+          <mesh
+            position={[0, 0.5, 0]}
+            castShadow
+            ref={sphereRef}
+            name="Blue ball"
+          >
             <sphereGeometry args={[0.5, 32, 32]} />
-            <meshStandardMaterial color={"#98F5F9"} />
+            <meshStandardMaterial color={blueState.clicked?blueState.toggledColor:blueState.color} />
           </mesh>
-
-          <mesh position={[2, 0.5, 0]} castShadow ref={sphereRef} name="Green ball">
+          <mesh
+            position={[2, 0.5, 0]}
+            castShadow
+            ref={sphereRef}
+            name="Green ball"
+          >
             <sphereGeometry args={[0.5, 32, 32]} />
-            <meshStandardMaterial color={"#a7f2a7"} />
+            <meshStandardMaterial color={greenState.clicked?greenState.toggledColor:greenState.color} />
           </mesh>
-          <mesh position={[-2, 0.5, 0]} castShadow ref={sphereRef} name="Red ball">
+          <mesh
+            position={[-2, 0.5, 0]}
+            castShadow
+            ref={sphereRef}
+            name="Red ball"
+          >
             <sphereGeometry args={[0.5, 32, 32]} />
-            <meshStandardMaterial color={"#C15151"} />
+            <meshStandardMaterial color={redState.clicked?redState.toggledColor:redState.color} />
           </mesh>
-
-          <mesh rotation={[anglesToRadians(-90), 0, 0]} receiveShadow name="floor">
+          <mesh
+            rotation={[anglesToRadians(-90), 0, 0]}
+            receiveShadow
+            name="Floor"
+          >
             <planeGeometry args={[50, 50]} />
             <meshStandardMaterial
-              color={"#1b4f3a"}
+              color={"#686D34"}
               metalness={0.1}
               roughness={0.1}
             />
@@ -119,25 +155,31 @@ function App() {
             decay={0.5}
             castShadow
           />
-
           <Environment background>
             <mesh>
               <sphereGeometry args={[50, 100, 10]} />
-              <meshBasicMaterial side={THREE.BackSide} color="#296777" />
+              <meshBasicMaterial side={THREE.BackSide} color="#6A6C53" />
             </mesh>
           </Environment>
         </Canvas>
-        <div className="flex-1 grid place-items-center">
-          <span>Message:</span>
-          <span>
-          {message}
-          </span>
-          </div>
+        <div className=" p-6 md:min-w-[250px] grid place-items-center">
+          <span className="font-bold text-lg">Message:</span>
+          <span className="font-semibold">{message}</span>
+        </div>
       </div>
     </>
   );
 }
-
 export default App;
 
 export const anglesToRadians = (degree: number) => degree * (Math.PI / 180);
+export const transObjY = (
+  obj: THREE.Object3D<THREE.Object3DEventMap>,
+  yDelta: number
+) => {
+  gsap.to(obj.position, {
+    y: obj.position.y + yDelta,
+    duration: 0.5,
+    ease: "power1.out",
+  });
+};
